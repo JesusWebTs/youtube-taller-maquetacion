a787a6c05af9d57d0f442474376f7addce5d5b31
RICHARD MARTINEZ <jesus.152006@gmail.com>
2020-12-30 14:11:45 -0400

    Subiendo Proyecto 1 Landing Page Portafolio-CV

---
+++ 1-proyecto-figma-jon-mircha/script.js
@@ -0,0 +1,58 @@
+/***** Menu ******/
+((d) => {
+  const $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".menu");
+
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+  });
+
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
+})(document);
+
+/**** ContactForm ****/
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+
+    fetch("https://formsubmit.co/ajax/jesus.152006@hotmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => {
+        res.ok ? res.json() : Promise.reject(res);
+      })
+      .then((json) => {
+        console.log(json);
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrio un error al enviar, intenta nuvamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error: ${err.status}: ${message}`;
+    })
+    .finally(() => {
+          location.hash = "#gracias";
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#cerrar";
+        }, 5000);
+      });
+  });
+})(document);
+
